667e201433bf2366202fb7006dd3ce67704e5a3b
Iggyfufu <kennethzhu07@gmail.com>
2018-06-25 11:15:12 -0400

    Fix(config) moved config files for firebase to a secret

--- functions/index.js
+++ functions/index.js
@@ -11,8 +11,6 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 exports.grabKatas = functions.https.onCall((req, res) => {
   const url = "https://www.codewars.com/api/v1/users/Iggyfufu?access_key=yascwivYsWnbjPgSmPxH"
   
-  // console.log(req);
-  
   return request(url, (error, response, body) => {
     console.log('asdfjlsafljwkerjwkrjksladf',response, body);
     
--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase'
-
+import config from '../config'
 // Initialize Firebase
-const config = {
-  apiKey: 'AIzaSyDTUyDmowu9Zx2a-HfLonbr3hTx5SGqzv4',
-  authDomain: 'fir-tutorial-f4f0f.firebaseapp.com',
-  databaseURL: 'https://fir-tutorial-f4f0f.firebaseio.com',
-  projectId: 'fir-tutorial-f4f0f',
-  storageBucket: 'fir-tutorial-f4f0f.appspot.com',
-  messagingSenderId: '576359877651',
-}
+
 firebase.initializeApp(config)
 
 export default firebase
