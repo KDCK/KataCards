78cfecfdd2e9a5eecadc7318e733929a499c1546
Iggyfufu <kennethzhu07@gmail.com>
2018-06-25 11:27:03 -0400

    Feat(firebase) Firebase Init

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,8 @@
 const functions = require('firebase-functions');
-const request = require('request')
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
-
-exports.grabKatas = functions.https.onCall((req, res) => {
-  const url = "https://www.codewars.com/api/v1/users/Iggyfufu?access_key=yascwivYsWnbjPgSmPxH"
-  
-  return request(url, (error, response, body) => {
-    console.log('asdfjlsafljwkerjwkrjksladf',response, body);
-    
-    res.send(body);
-  });
-})
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
