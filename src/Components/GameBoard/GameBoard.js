74069fd855f42400bf9fda3235742b1d13930cd8
cardeljan <cardeljan@gmail.com>
2018-06-26 16:41:53 -0400

    Styling(GameBoard): Added color gradient, <hr />

--- src/Components/GameBoard/GameBoard.js
+++ src/Components/GameBoard/GameBoard.js
@@ -1,8 +1,29 @@
 import React, {Component} from 'react'
+import './GameBoard.css'
 
 class GameBoard extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
   render() {
-    return <h1>I'm a gameboard. Let's play</h1>
+    return (
+      <div className="game-container">
+        <div className="player1-board-deck">
+          <h1>Player1 Deck Placeholder</h1>
+        </div>
+        <div className="gameboard-player1">
+          <h1>P1 GameBoard Placeholder</h1>
+        </div>
+        <hr />
+        <div className="gameboard-player2">
+          <h1>P2 GameBoard Placeholder</h1>
+        </div>
+        <div className="player2-board-deck">
+          <h1>Player2 Deck Placeholder</h1>
+        </div>
+      </div>
+    )
   }
 }
 
