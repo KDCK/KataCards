d7ee3ca872b733e84658bbdc5563c122d2d3f9d5
Daniel Howard <danielhoward314@gmail.com>
2018-06-26 11:15:04 -0400

    feat(signup and login components) adds redirect to home for successful logins and signups

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import {Route, Switch} from 'react-router-dom'
-import {Home, Login, Signup, Logout, Nav} from './Components'
+import {Home, Login, Signup, Logout, Nav, Profile} from './Components'
 import SingleCard from './Components/Cards/SingleCard'
 import AllCards from './Components/Cards/AllCards'
 
@@ -18,6 +18,7 @@ class Routes extends Component {
           <Route exact path="/home" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
+          <Route exact path="/profile" component={Profile} />
           <Route exact path="/logout" component={Logout} /> {/* maybe we replace w/ a splash screen */}
         </Switch>
       </div>
