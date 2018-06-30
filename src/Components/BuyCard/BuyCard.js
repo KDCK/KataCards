ddf59c276d91e86a740f1294499c641e1681a2ba
cardeljan <cardeljan@gmail.com>
2018-06-30 15:02:29 -0400

    Styling(BuyCard): Fixed re-render positioning

--- src/Components/BuyCard/BuyCard.js
+++ src/Components/BuyCard/BuyCard.js
@@ -62,10 +62,10 @@ class BuyCard extends Component {
         <img className="store-img" alt="home background" src="cardstore.gif" />
         <div className="store-container">
           <h1>Welcome to the Card Store</h1>
-          <p>
+          <h4>
             Purchasing from a higher Tier increases the likelihood of getting a
             better card, but does not guarantee it!
-          </p>
+          </h4>
           <h3>You have {this.props.user.gold} gold</h3>
           <h3>How much do you want to spend?</h3>
           <p>
@@ -79,18 +79,21 @@ class BuyCard extends Component {
         </div>
       </div>
     ) : (
-      <div className="store-bought">
-        <h1>You bought: {this.state.purchasedCard.name}!</h1>
-        <h2>Tier {this.state.purchasedCard.tier}</h2>
-        <SingleCard card={this.state.purchasedCard} />
-        <h3>You have {this.props.user.gold} gold left</h3>
-        <Button
-          onClick={this.backToStore}
-          className="back-to-store"
-          waves="red"
-        >
-          Try Again?
-        </Button>
+      <div>
+        <img className="store-img" alt="home background" src="cardstore.gif" />
+        <div className="store-bought">
+          <h1>You bought: {this.state.purchasedCard.name}!</h1>
+          <h2>Tier {this.state.purchasedCard.tier}</h2>
+          <SingleCard card={this.state.purchasedCard} />
+          <h3>You have {this.props.user.gold} gold left</h3>
+          <Button
+            onClick={this.backToStore}
+            className="back-to-store"
+            waves="red"
+          >
+            Try Again?
+          </Button>
+        </div>
       </div>
     )
   }
