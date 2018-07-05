f71294b63b14b7adb3fb72827ef3c508eb2228d7
Iggyfufu <kennethzhu07@gmail.com>
2018-07-05 15:27:19 -0400

    Fix(CORS)

--- server/server.js
+++ server/server.js
@@ -1,23 +1,25 @@
 const express = require('express');
 const axios = require('axios')
+const cors = require('cors')
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
-})
+var corsOptions = {
+  origin: 'https://codecards-53f05.firebaseapp.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
-app.get('/api/code/:userhandle', async (req, res) => {
+app.use(cors())
+
+app.get('/api/code/:userhandle', cors(corsOptions), async (req, res) => {
   axios.get(`https://www.codewars.com/api/v1/users/${req.params.userhandle}?access_key=yascwivYsWnbjPgSmPxH`)
     .then(result => res.send(result.data))
 });
 
-app.get('/api/code/validate/:userhandle', (req, res) => {
+app.get('/api/code/validate/:userhandle', cors(corsOptions), (req, res) => {
   console.log(req, res);
-  
+
   axios.get(`https://www.codewars.com/api/v1/users/${req.params.userhandle}?access_key=yascwivYsWnbjPgSmPxH`)
     .then(() => res.send(true))
     .catch(() => res.send(false))
